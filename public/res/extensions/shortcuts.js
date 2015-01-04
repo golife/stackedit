ccb249ea63ca2045a725fa29eb60aa40c212fe23
life <lifephp@gmail.com>
2015-01-05 01:06:37 +0800

    快捷方式 ok

--- public/res/extensions/shortcuts.js
+++ public/res/extensions/shortcuts.js
@@ -55,9 +55,48 @@ define([
 		});
 	}
 
+	/*
+	
+    'mod+b': bindPagedownButton('bold'),
+    'mod+i': bindPagedownButton('italic'),
+    'mod+l': bindPagedownButton('link'),
+    'mod+q': bindPagedownButton('quote'),
+    'mod+k': bindPagedownButton('code'),
+    'mod+g': bindPagedownButton('image'),
+    'mod+o': bindPagedownButton('olist'),
+    'mod+u': bindPagedownButton('ulist'),
+    'mod+h': bindPagedownButton('heading'),
+    'mod+r': bindPagedownButton('hr'),
+    'mod+z': bindPagedownButton('undo'),
+    'mod+y': bindPagedownButton('redo'),
+    'mod+shift+z': bindPagedownButton('redo'),
+    'mod+m': function(evt) {
+        $('.button-open-discussion').click();
+        evt.preventDefault();
+    },
+    '= = > space': function() {
+        expand('==> ', '⇒ ');
+    },
+    '< = = space': function() {
+        expand('<== ', '⇐ ');
+    },
+    'S t a c k E d i t': function() {
+        eventMgr.onMessage("You are stunned!!! Aren't you?");
+    }
+	*/
 	/*jshint unused:true */
 	shortcuts.onInit = function() {
 		try {
+			// http://craig.is/killing/mice
+			// 只在输入框下有效, life
+			// http://stackoverflow.com/questions/16169645/using-mousetrap-on-a-specific-element
+			var input = $('.editor-content');
+			var _oldStopCallback = Mousetrap.stopCallback;
+			Mousetrap.stopCallback = function(e, element, combo) {
+		        return element !== input[0];
+			    return _oldStopCallback(e, element, combo);
+			}
+
 			/*jshint evil: true */
 			var shortcutMap;
 			eval('shortcutMap = ' + shortcuts.config.mapping);
